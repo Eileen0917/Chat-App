b32b5ef337c94cdcf94a373f94416c1e318eaab9
Eileen Kuo <j405307@gmail.com>
2019-03-26 21:05:59 +0800

    socket.io

--- src/app.js
+++ src/app.js
@@ -1,13 +1,2 @@
-const express = require('express')
-conat path = require('path')
-//require('./db/mongoose')
-
-const app = express()
-
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
 module.exports = app
